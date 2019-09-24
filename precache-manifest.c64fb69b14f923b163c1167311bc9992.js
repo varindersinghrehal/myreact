d6efc7866b5689b4360045e7333782a267a7e264
Varinder Singh Rehal <varinderbabbu555@gmail.com>
2019-09-24 19:21:23 +0200

    Updates

---
+++ precache-manifest.c64fb69b14f923b163c1167311bc9992.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20645cdd4cf688c7e36b78289f13f412",
+    "url": "/myreact/index.html"
+  },
+  {
+    "revision": "db7d5a9500d9b991c0c5",
+    "url": "/myreact/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "107fc01b760efb561106",
+    "url": "/myreact/static/js/2.a4813e05.chunk.js"
+  },
+  {
+    "revision": "db7d5a9500d9b991c0c5",
+    "url": "/myreact/static/js/main.dce716d7.chunk.js"
+  },
+  {
+    "revision": "c7707bd38196d70ea2c3",
+    "url": "/myreact/static/js/runtime-main.ff1a4b59.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/myreact/static/media/logo.25bf045c.svg"
+  }
+]);
